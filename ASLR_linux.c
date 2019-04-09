1028f2ea2c4003848422510629d622fc124c9edc
Raquel Vázquez Díaz <raquel.vazquez1@udc.es>
2019-04-09 19:49:15 +0200

    fixed and completed first version of code for linux

--- ASLR_linux.c
+++ ASLR_linux.c
@@ -1,3 +1,5 @@
+// para que los segmentos de texto, datos y BSS sean también aleatorios,
+// hay que compilar con la opción -fPIE -pie: gcc -fPIE -pie -o output ASLR_linux.c
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,13 +9,50 @@
 #include <unistd.h>
 #define MAX_PATH 255
 
+//se comprueba el comienzo del código (text segment)
 int verifyCode(FILE *fileCSV)
 {
-	fprintf(stdout,"%p\n",verifyCode);
-	fprintf(fileCSV,"%p\n",verifyCode);
+  extern void _start();
+	fprintf(stdout,"%p\n",&_start);
+	fprintf(fileCSV,"%p\n",&_start);
 	return 0;
 }
 
+//se comprueba una dirección del segmento de datos (data segment)
+int verifyData(FILE *fileCSV)
+{
+  static int staticVar = 3;
+	fprintf(stdout,"%p,",&staticVar);
+	fprintf(fileCSV,"%p,",&staticVar);
+	return 0;
+}
+
+//se comprueba una dirección del BSS, donde están las variables no inicializadas
+int verifyBSS(FILE *fileCSV)
+{
+  static char *uninitVar;
+	fprintf(stdout,"%p,",&uninitVar);
+	fprintf(fileCSV,"%p,",&uninitVar);
+	return 0;
+}
+
+//se comprueba el comienzo del heap
+int verifyHeap(FILE *fileCSV)
+{
+	char *mallocVar;
+	mallocVar=(char *) malloc(100);
+	if(mallocVar==NULL)
+  {
+		fprintf(stdout,"error,");
+		return 1;
+	} 
+  fprintf(stdout,"%p,",mallocVar);
+  fprintf(fileCSV,"%p,",mallocVar);
+  free(mallocVar);
+	return 0;
+}
+
+//se comprueba el comienzo del brk (final del heap)
 int verifyBrk(FILE *fileCSV)
 {
   void *b = sbrk(0);
@@ -24,14 +63,7 @@ int verifyBrk(FILE *fileCSV)
   return 0;
 }
 
-int verifyVDSO(FILE *fileCSV)
-{
-  unsigned long vdso = getauxval(AT_SYSINFO_EHDR);
-	fprintf(stdout,"%p,",(unsigned long*) vdso);
-	fprintf(fileCSV,"%p,",(unsigned long*) vdso);
-	return 0;
-}
-
+//se comprueba una direción de la zona de memoria compartida
 int verifyMmap(FILE *fileCSV)
 {
   void *p;
@@ -48,26 +80,21 @@ int verifyMmap(FILE *fileCSV)
 	return 0;
 }
 
-int verifyHeap(FILE *fileCSV)
-{
-	char *mallocVar;
-	mallocVar=(char *) malloc(100);
-	if(mallocVar==NULL)
-  {
-		fprintf(stdout,"error,");
-		return 1;
-	} 
-  fprintf(stdout,"%p,",mallocVar);
-  fprintf(fileCSV,"%p,",mallocVar);
-  free(mallocVar);
-	return 0;
-}
-
+//se comprueba la dirección de la pila
 int verifyStack(FILE *fileCSV)
 {
 	int stack=1;
 	fprintf(stdout,"%p,",&stack);
 	fprintf(fileCSV,"%p,",&stack);
+	return 0;
+}
+
+//se comprueba la dirección de un Virtual Dynamic Shared Object
+int verifyVDSO(FILE *fileCSV)
+{
+  unsigned long vdso = getauxval(AT_SYSINFO_EHDR);
+	fprintf(stdout,"%p,",(unsigned long*) vdso);
+	fprintf(fileCSV,"%p,",(unsigned long*) vdso);
 	return 0;
 }
 
@@ -91,15 +118,20 @@ int main(int argc, char *argv[])
     printf("Cannot open an input file. Exiting.\n");
     return 1;
   }
-  printf("+-------+--------+--------+--------+--------+--------+\n");
-  printf(" VDSO | Stack | Mmap | Heap | Brk | Code \n");
-  printf("+-------+--------+--------+--------+--------+--------+\n");
+  printf("+-------------+--------------+--------------+--------------+" 
+    "--------------+--------------+--------------+-------------+\n");
+  printf("     VDSO     |    Stack     |     Mmap     |     Brk      |"
+    "     Heap     |     BSS      |     Data     |     Code     \n");
+  printf("+-------------+--------------+--------------+--------------+"
+    "--------------+--------------+--------------+-------------+\n");
 
 	verifyVDSO(fileCSV);
   verifyStack(fileCSV);
 	verifyMmap(fileCSV);
-	verifyHeap(fileCSV);
   verifyBrk(fileCSV);
+	verifyHeap(fileCSV);
+  verifyBSS(fileCSV);
+  verifyData(fileCSV); 
   verifyCode(fileCSV);
   fclose(fileCSV);	
   return 0; 
